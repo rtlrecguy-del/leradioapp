4b324ddcbf402052e9634554a4696883a2812182
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-02 20:48:07 -0500

    Delete myapp2Arm directory

--- myapp2Arm/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-    cron -f &
-
-    exec apache2ctl -D FOREGROUND
--- myapp2Arm/www/scripts/tuneincron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-crontab -l > /var/www/html/scripts/varcron/mycron5
-#echo new cron into cron file
-echo "$5 $2 * * $3 /var/www/html/scripts/tuneinradio.sh $1 $4" >> /var/www/html/scripts/varcron/mycron5
-#install new cron file
-crontab /var/www/html/scripts/varcron/mycron5
-rm /var/www/html/scripts/varcron/mycron5
