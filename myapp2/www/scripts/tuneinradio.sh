7cc2b0cbd4dd8debc8cbeaa76d7dce4bce201f6a
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 14:22:19 -0500

    Update tuneinradio.sh

--- myapp2/www/scripts/tuneinradio.sh
+++ myapp2/www/scripts/tuneinradio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ted
+cd /var/www/html/recordings
 current_time=$(date "+%d-%H.%M") > /dev/null 2>&1
 if [ $1 = "1" ]; then
 String98="timeout 1h mpv --quiet --vo=null -ao=null --stream-record=/var/www/html/recordings/tunein$current_time.mp3 http://opml.radiotime.com/Tune.ashx?id=s20431&formats=aac,mp3 > /dev/null 2>&1 &"
@@ -14,5 +14,6 @@ String98="timeout 1h mpv --quiet --vo=null -ao=null --stream-record=/var/www/htm
 eval "$String98"
 fi
 if [ $1 = "4" ]; then
-String98="timeout 1h mpv --quiet --vo=null -ao=null --stream-record=/var/www/html/recordings/tunein$current_time.mp3 http://opml.radiotime.com/Tune.ashx?id=${1}&formats=aac,mp3 > /dev/nul>eval "$String98"
+String98="timeout 1h mpv --quiet --vo=null -ao=null --stream-record=/var/www/html/recordings/tunein$current_time.mp3 http://opml.radiotime.com/Tune.ashx?id=${1}&formats=aac,mp3 > /dev/null 2>&1 &"
+eval "$String98"
 fi
