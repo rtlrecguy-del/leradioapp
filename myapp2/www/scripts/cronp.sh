2d63275f7b0a737e1a71515e44000abd6a0cc8f8
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 11:29:26 -0500

    Update cronp.sh

--- myapp2/www/scripts/cronp.sh
+++ myapp2/www/scripts/cronp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 [[ -z "$1" ]] && { echo "Parameter 1 is empty" ; exit 1; }
-crontab -l | grep -v $1 > /var/www/html/scripts/varcron/mycron
+crontab -l | sed "$1d" > /var/www/html/scripts/varcron/mycron
 #echo new cron into cron file
 crontab /var/www/html/scripts/varcron/mycron
 rm /var/www/html/scripts/varcron/mycron
+
