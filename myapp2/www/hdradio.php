8c9768ca3481de7cd04d3b5ef4aa6010f56f1686
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-03 22:22:29 -0500

    Update hdradio.php

--- myapp2/www/hdradio.php
+++ myapp2/www/hdradio.php
@@ -4,6 +4,12 @@ $message3=shell_exec('echo "test"');
    echo "<pre>$message3</pre>";
 }
 if(array_key_exists('dcron', $_POST)) {
+$command = "sudo ps aux | grep ffmpeg | grep -v grep";
+$output = shell_exec($command);
+
+if (!empty($output)) {
+    echo "The program '" . $programName . "' is running already.\n";
+} 
 $varfreq1=$_POST['freq1'];
 $ip=$_SERVER['REMOTE_ADDR'];
 
