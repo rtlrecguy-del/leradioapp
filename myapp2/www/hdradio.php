3e9dffb0446dc018d430312774933b60e1b842ca
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-03 22:10:32 -0500

    Update hdradio.php

--- myapp2/www/hdradio.php
+++ myapp2/www/hdradio.php
@@ -23,7 +23,7 @@ $varspace=" ";
    $varpatternfreq1="/^\d{1,2}\.\d{1}$/";
    $varpatternfreq="/^\d{1,2}\.\d{1}$/";
    $varpatternsubstation="/[1-4]/";
-   $varpatternip="'/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/'";
+   $varpatternip="/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/";
 $f=$varsudo.$varspace.$varcommand.$varspace.$varcommandarams.$varspace.$varsubstation.$varspace.$ip;
    if (preg_match($pattern1, $varsudo) && preg_match($pattern2, $varcommand) && preg_match($pattern1, $varfreq1) && preg_match($pattern2, $varfreq)) && preg_match($varpatternip, $ip) && preg_match($varpatternsubstation, $varsubstation)) {
   echo "Successfully Started.  Open VLC from client to address udp://@0.0.0.0:12345";
