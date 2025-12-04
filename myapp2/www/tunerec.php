e150160eed8c50111653b9925a10d3eaaeb3eb70
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-03 23:00:02 -0500

    Update tunerec.php

--- myapp2/www/tunerec.php
+++ myapp2/www/tunerec.php
@@ -21,7 +21,7 @@ $f=$varsudo.$varspace.$command.$varspace.$varstation.$varspace.$varhour.$varspac
    $varpatternstation="/[1-4]/";
    $varpatternfreq="/^\d{1,2}\.\d{1}$/";
    $varpatternhour="/[1-24]/";
-   $varpatternminute="/[1-60]/;
+   $varpatternminute="/[1-60]/";
    $varpatterndayofshow="/^.{1,3}$/";
 if (preg_match($varpatternsudo, $varsudo) && preg_match($varpatterncommand, $varcommand) && preg_match($varpatternstation,$varstation) && preg_match($varpatternhour, $varhour) 
    && preg_match($varpatternminute, $varminute) && preg_match($varpatternhour, $varhour) && preg_match($varpatterndayofshow, $vardayofshow) && preg_match($varpatterntinid, $vartinid)) {
