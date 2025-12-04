b6e88dec8fcc0080974fc2f793313f963714bb13
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-03 23:24:30 -0500

    Update sradio.php

--- myapp2/www/sradio.php
+++ myapp2/www/sradio.php
@@ -29,12 +29,12 @@ $f=$varsudo.$varspace.$varcommand.$varspace.$varfreq1.$vardot.$varfreq.$varspace
    $varpatternfreq1="/^\d{1,2}\.\d{1}$/";
    $varpatternfreq="/^\d{1,2}\.\d{1}$/";
    $varpatternip="/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/";
-if (preg_match($pattern1, $varsudo) && preg_match($pattern2, $varcommand) && preg_match($pattern1, $varfreq1) && preg_match($pattern2, $varfreq)) && preg_match($varpatternip, $ip)) {
+if (preg_match($pattern1, $varsudo) && preg_match($pattern2, $varcommand) && preg_match($pattern1, $varfreq1) && preg_match($pattern2, $varfreq) && preg_match($varpatternip, $ip)) {
   echo "Successfully Started Open VLC on client at address udp://@0.0.0.0:12345";
    $message3=shell_exec($f);
    echo "<pre>$message3</pre>";
 }
-echo {
+else {
    echo "Input did not validate";
 }}
 if(array_key_exists('dstop', $_POST)) {
