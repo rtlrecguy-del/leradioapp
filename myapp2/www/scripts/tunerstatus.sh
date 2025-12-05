495640b41adc604e4fa5db355bbe357a3e277a39
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 15:33:38 -0500

    Update tunerstatus.sh

--- myapp2/www/scripts/tunerstatus.sh
+++ myapp2/www/scripts/tunerstatus.sh
@@ -7,7 +7,6 @@ new_string=$(echo "$line" | sed 's/-s 200000 -r 48000//g')
 done
 count=0
 /usr/bin/ps -eo command | grep nrsc5 | grep -v "grep" | while IFS= read -r line; do
-new_string=$(echo "$line" | sed 's/-s 200000 -r 48000//g')
-    echo "status tuner $count: $new_string"
+    echo "status tuner $count: $line"
   ((count++))
 done
