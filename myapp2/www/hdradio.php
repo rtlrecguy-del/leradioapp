4ed8caa0db67cc14234cf74b0da97979a2373044
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 20:12:40 -0500

    Update hdradio.php

--- myapp2/www/hdradio.php
+++ myapp2/www/hdradio.php
@@ -65,12 +65,15 @@ fillSelect("../txtfiles/sub.txt","substation")
 </script>
 <label for="selectbox"">Goto WebPage:</label>
 <select id="selectbox" name="" onchange="javascript:location.href = this.value;">
-     <option value="../index.php">Record SD FM</option>
-    <option value="../hdindex.php">Record HD Radio</option>
+    <option value="">Choose Page To Go To:</option>
+    <option value="../index.php">Welcome Page</option>
+    <option value="../fmrecord.php">Record SD FM</option>
+    <option value="../hdrecord.php">Record HD Radio</option>
     <option value="../cron.php">Manage Recordings</option>
-    <option value="../sradio.php">Stream FM Radio</option>
+    <option value="../fmradio.php">Stream FM Radio</option>
   <option value="../hdradio.php">Stream HD Radio</option>
 </select>
+
 </br>
 <form action="" method="post">
 <label for="station"">Frequency:</label>
