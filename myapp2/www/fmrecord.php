8ea3be7fa88ed187eb1de1c9601d6b8c217ab62c
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 16:42:10 -0500

    Update fmrecord.php

--- myapp2/www/fmrecord.php
+++ myapp2/www/fmrecord.php
@@ -119,6 +119,17 @@ fillSelect("../txtfiles/cday.txt","dayofshow");
 </br></br>
 
 <input type="submit" name="dcron" value="Record FM"/>
+<?php
+$command_output=shell_exec('sudo /bin/bash /var/www/html/scripts/tunerpid.sh');
+<select name="tunerselect" id="tunerselect">
+            <option value="">Select which tuner to stop</option>
+       $lines = explode(PHP_EOL, $output);
+
+    // Loop through each line and process it
+    foreach ($lines as $line) {
+                echo "<option value=\"". htmlspecialchars($line) ."\">". htmlspecialchars($line) ."</option>";
+    } </select>
+?>   
 <input type="submit" name="stoprec" value="Stop Recording"/>
 </form>
 </body>
