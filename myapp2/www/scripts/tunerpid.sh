d332243d616df50939fa29a62e6cee083194c1b0
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 16:34:52 -0500

    Update tunerpid.sh

--- myapp2/www/scripts/tunerpid.sh
+++ myapp2/www/scripts/tunerpid.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-count=0
 /usr/bin/ps -eo pid | grep rtl_fm | grep -v "grep" | while IFS= read -r line; do
 new_string=$(echo "$line" | sed 's/-s 200000 -r 48000//g')
-    echo "status tuner $count: $new_string"
-  ((count++))
+    echo "$new_string"
+    echo "$new_string"
 done
-count=0
 /usr/bin/ps -eo pid | grep nrsc5 | grep -v "grep" | while IFS= read -r line; do
-    echo "status tuner $count: $line"
-  ((count++))
+    echo "$line"
+    echo "$line"
 done
