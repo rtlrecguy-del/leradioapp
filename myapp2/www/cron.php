963f647636d37135e3956a17f15ac13c7e06e1a7
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 20:52:39 -0500

    Update cron.php

--- myapp2/www/cron.php
+++ myapp2/www/cron.php
@@ -10,7 +10,7 @@ $message3=shell_exec('crontab -l | cat -n');
 if(array_key_exists('dcronbtn', $_POST)) {
 $vardel=$_POST['del'];
 $varspace=" ";
-$varcommand="/bin/bash /var/www/html/scripts/cronp.sh";
+$varcommand="/bin/bash /var/www/html/scripts/delcron.sh";
 $f=$varcommand.$varspace.$vardel;
 $vardelpattern="/^(?:[1-9]|[1-5]\d|24)$/";
 if (preg_match($vardelpattern, $vardel)) {
