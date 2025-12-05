302464498743450a97135e9ec8d31870d5937b9c
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 13:07:31 -0500

    Update sigint.sh

--- myapp2/www/scripts/sigint.sh
+++ myapp2/www/scripts/sigint.sh
@@ -1,10 +1,6 @@
- #!/bin/bash
-USER=www-data
-ffmpegpid=$(/bin/ps -fu $USER| grep "ffmpeg" | grep -v "grep" | awk '{print $2}'`)
-nrsc5id=$(/bin/ps -fu $USER| grep "nrsc5" | grep -v "grep" | awk '{print $2}'`)
-rtl_smid=$(/bin/ps -fu $USER| grep "rtl_fm" | grep -v "grep" | awk '{print $2}'`)
- 
-kill -SIGINT "$ffmpegpid"
-kill -SIGINT "$rtl_fmpid"
-kill -SIGINT "$nrsc5pid"
+#!/bin/bash
+ffmpegpid=$(pidof ffmpeg)
+if [[ -n "$ffmpegpid" ]]; then
+kill -s SIGINT "$ffmpegpid"
+fi
   
