4c25fd567f22d15c7a8c5e79eca844b32cefd648
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 20:29:42 -0500

    Update index.php

--- myapp2/www/index.php
+++ myapp2/www/index.php
@@ -1,57 +1,6 @@
 <?php
-if(isset($_POST['submit'])) {
-$message3=shell_exec('echo "test"');
-   echo "<pre>$message3</pre>";
-}
-
-if(array_key_exists('dcron', $_POST)) {
-
-$varfreq1=$_POST['freq'];
-$varfreq=$_POST['freq1'];
-$varcommanderiod=".";
-$varcommandarams=$varfreq.$varcommanderiod.$varfreq1;
-
-
-
-$vargain=$_POST['gain'];
-
-
-
-
- $varfreq=$_POST['freq'];
-$varhour=$_POST['hour'];
-$vardayofshow=$_POST['dayofshow'];
-$vargain=$_POST['gain'];
-$varminute=$_POST['minute'];
-
-
-$varcommand="/bin/bash /var/www/html/scripts/cron.sh";
-$varspace=" ";
-$f=$varcommand.$varspace.$varcommandarams.$varspace.$varhour.$varspace.$vardayofshow.$varspace.$vargain.$varspace.$varminute;
-   $varpatternfreq1="/^\d{1,3}$/";
-   $vargain="/^\d{1,2}\.\d{1}$/";
-   $varpatternfreq="/^\d{1,3}$/";
-   $varpatternhour="/^(?:[1-9]|[1-5]\d|24)$/";
-   $varpatternminute="/^(?:[1-9]|[1-5]\d|60)$/";
-   $varpatterndayofshow="/^.{1,3}$/";
-if ((preg_match($varpatternfreq1, $varfreq1) && preg_match($varpatternfreq, $varfreq) && preg_match($varpatternhour, $varhour) && preg_match($varpatternminute, $varminute) && preg_match($varpatternhour, $varhour) && preg_match($varpatterndayofshow, $vardayofshow))) {
-  echo "Successfully Scheduled Recording";
-   $message3=shell_exec($f);
-   echo "<pre>$message3</pre>";
-}
-else { 
-   echo "Your input didn't validate";
-}
-   
-   
-
-}
-
-
-?>
-
 <html>
-<body>
+<body style="background-color: blue;"><body>
 <head>
  <script src="../script.js"></script> 
 <link rel="stylesheet" href="../styles.css">
@@ -69,48 +18,15 @@ fillSelect("../txtfiles/cday.txt","dayofshow");
 </script>
 <select id="selectbox" name="" onchange="javascript:location.href = this.value;">
 <option value="">Choose Page To Go To:</option>
-    <option value="../index.php">Record FM</option>
-    <option value="../hdindex.php">Record HD Radio</option>
+     <option value="../index.php">Welcome Page</option>
+    <option value="../fmrecord.php">Record SD FM</option>
+    <option value="../hdrecord.php">Record HD Radio</option>
     <option value="../cron.php">Manage Recordings</option>
-    <option value="../sradio.php">Stream FM Radio</option>
+    <option value="../fmradio.php">Stream FM Radio</option>
   <option value="../hdradio.php">Stream HD Radio</option>
 </select>
+<p1> Welcome to The OTA DVR like Device for Radio  If you haven't already pleast sign in</p1>
 
-</br>
-<form action="" method="post">
-<label for="freq">Frequency:</label>
-<div class="row">
-<select style="width:20%;" name="freq1" id="freq1">
-</select>
-<select style="width:20%;" name="freq" id="freq">
-</select>
-</div>
-
-
-<label for="minute">minute:</label>
-<select name="minute" id="minute">
-</select>
-
-
-
-<label for="hour">hour:</label>
-
-<select name="hour" id="hour">
-</select>
-<label for="day of show">day of show</label>
-<select name="dayofshow" id="dayofshow">
-</select>
-
-
-<label for="gain">gain:</label>
-<select name="gain" id="gain">
-</select>
-
-</br></br>
-
-<input type="submit" name="dcron" value="Record FM"/>
-
-</form>
 </body>
 </html>
 
