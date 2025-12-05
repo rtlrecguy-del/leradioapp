85dd93cf58790b9176257a508bf766ef64842b11
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 20:11:58 -0500

    Update and rename hdindex.php to hdirecord.php

--- myapp2/www/hdirecord.php
+++ myapp2/www/hdirecord.php
@@ -56,10 +56,11 @@ else {
 <label for="selectbox">Goto WebPage:</label>
 <select id="selectbox" name="" onchange="javascript:location.href = this.value;">
 <option value="">Choose Page To Go To:</option>
-    <option value="../index.php">Record SD FM</option>
-    <option value="../hdindex.php">Record HD Radio</option>
+    <option value="../index.php">Welcome Page</option>
+    <option value="../fmrecord.php">Record SD FM</option>
+    <option value="../hdrecord.php">Record HD Radio</option>
     <option value="../cron.php">Manage Recordings</option>
-    <option value="../sradio.php">Stream FM Radio</option>
+    <option value="../fmradio.php">Stream FM Radio</option>
   <option value="../hdradio.php">Stream HD Radio</option>
 </select>
 </br>
