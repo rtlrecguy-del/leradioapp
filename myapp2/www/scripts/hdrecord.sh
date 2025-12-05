debe491d3c8386290ce5c65d76e55d59fc7ccf4d
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-04 20:58:55 -0500

    Update hdrecord.sh

--- myapp2/www/scripts/hdrecord.sh
+++ myapp2/www/scripts/hdrecord.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-num=$(awk "BEGIN {print $1+125; exit}")
 current_time=$(date "+%d-%H.%M") > /dev/null 2>&1
-String98="nrsc5 $1 $2 -o - 2> /dev/null | ffmpeg -t 3600 -f s16le -ar 92000 -i pipe:0 -af arnndn=m=/var/www/html/noisemod/rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn /var/www/html/recordings/$1showat$current_time.mp3 > /dev/null 2>&1 &"
-eval "$String98"
+command="nrsc5 $1 $2 -o - 2> /dev/null | ffmpeg -t 3600 -f s16le -ar 92000 -i pipe:0 -af arnndn=m=/var/www/html/noisemod/rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn /var/www/html/recordings/$1showat$current_time.mp3 > /dev/null 2>&1 &"
+eval "$command"
