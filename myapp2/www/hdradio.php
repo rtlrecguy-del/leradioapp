57e811bb71f16f582d0a94f0141d34ce8eabd175
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 17:27:21 -0500

    Update hdradio.php

--- myapp2/www/hdradio.php
+++ myapp2/www/hdradio.php
@@ -50,12 +50,12 @@ if(array_key_exists('stopplay', $_POST)) {
 $command="sudo /bin/bash /var/www/html/scripts/sigint.sh";
 $varspace=" ";
  $varpatterntunerselect="/^\d{1,6}$/";
-if ((preg_match($varpatterntunerselect, $vartunerselect_sanitized){
+if ((preg_match($varpatterntunerselect, $vartunerselect_sanitized)){
 $f=$command.$varspace.$vartunerselect_sanitized;
 $message3=shell_exec($f);
    echo "<pre>$message3</pre>";
 }
-
+}
 
 ?>
 <html>
