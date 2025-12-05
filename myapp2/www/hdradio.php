9bf13eb525a8a3ac21d797bbaabd71548c87e349
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 09:39:31 -0500

    Update hdradio.php

--- myapp2/www/hdradio.php
+++ myapp2/www/hdradio.php
@@ -6,10 +6,10 @@ echo $sanitized_output;
 }
 exec("pgrep ffmpeg", $output, $return);
 if ($return == 0) {
-   echo 'Recording ... <button class="red-button">Click Me</button>';
+   echo 'Recording/streaming ... <button class="red-button">Click Me</button>';
 }
 else {
-   echo 'Not Recording ... <button class="green-button">Click Me</button>';
+   echo 'Not Recording/streaming ... <button class="green-button">Click Me</button>';
 }
 if(array_key_exists('dcron', $_POST)) {
 
