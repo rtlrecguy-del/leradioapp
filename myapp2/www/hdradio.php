a5fcbd1a775869bda06145717358bcb31491a930
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 15:38:19 -0500

    Update hdradio.php

--- myapp2/www/hdradio.php
+++ myapp2/www/hdradio.php
@@ -1,7 +1,8 @@
 <?php
 if(isset($_POST['submit'])) {
-$command_output = shell_exec('echo "test');
+$command_output=shell_exec('sudo /bin/bash /var/www/html/scripts/tunerstatus.sh');
 $sanitized_output = htmlspecialchars($command_output, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+echo $sanitized_output;
 }
 if(array_key_exists('dcron', $_POST)) {
 
