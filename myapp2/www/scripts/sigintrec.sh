c099369f1ce89476fadb4c69a62f3272cb27e88d
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 21:45:34 -0500

    Update sigintrec.sh

--- myapp2/www/scripts/sigintrec.sh
+++ myapp2/www/scripts/sigintrec.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-/usr/bin/ps -eo pid,command | grep ffmpeg | grep "/var/www/html/scripts/recordings" | while IFS= read -r line; do
-pid_awk=$(echo "$line" | awk '{print $1}')
-if [[ -n "$pid_awk" ]]; then
-kill -s SIGINT "$pid_awk"
-fi
-done
-echo "$pid_awk"
+pkill -9 nrsc5 
