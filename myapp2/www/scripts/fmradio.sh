081567f7200d9310a0c1e0a6e2d7053745277251
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-06 17:42:48 -0500

    Update fmradio.sh

--- myapp2/www/scripts/fmradio.sh
+++ myapp2/www/scripts/fmradio.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-command="rtl_fm -f $1M -s 200000 -r 48000 -g 49.6 2> /dev/null | ffmpeg -re -f s16le -ar 48000 -i pipe:0 -c:v libx264 -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename \"segment%03d.ts\" output.m3u8 > /dev/null 2>&1 &"
+cd hls
+command="rtl_fm -f $1M -s 200000 -r 48000 -g 49.6 2> /dev/null | ffmpeg -re -f s16le -ar 48000 -i pipe:0 -c:v libx264 -preset veryfast -hls_time 10 -hls_list_size 5 -hls_flags delete_segments output.m3u8 > /dev/null 2>&1 &"
 eval "$command"
