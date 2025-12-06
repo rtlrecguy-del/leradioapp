fd6df8405a85d37f71f35dd8c5872a9daf685807
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 22:13:31 -0500

    Update stopsdradio.sh

--- myapp2/www/scripts/stopsdradio.sh
+++ myapp2/www/scripts/stopsdradio.sh
@@ -1,4 +1,4 @@
-/usr/bin/ps -eo pid,command | grep ffmpeg | grep "udp://" | -v "grep" | while IFS= read -r line; do
+/usr/bin/ps -eo pid,command | grep ffmpeg | grep "udp://" | while IFS= read -r line; do
 pid_awk=$(echo "$line" | awk '{print $1}')
 if [[ -n "$pid_awk" ]]; then
 kill -s SIGINT "$pid_awk"
