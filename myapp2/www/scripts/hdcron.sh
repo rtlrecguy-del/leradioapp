b8ffa9492cb6c3be930d38becef9205832197cdf
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-06 00:49:11 -0500

    Update hdcron.sh

--- myapp2/www/scripts/hdcron.sh
+++ myapp2/www/scripts/hdcron.sh
@@ -2,7 +2,7 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 crontab -l > /var/www/html/scripts/varcron/mycron
 #echo new cron into cron file
-echo "$5 $2 * * $3 sudo /bin/bash /var/www/html/scripts/hdrecord.sh $1 $4" >> /var/www/html/scripts/varcron/mycron
+echo "$4 $3 * * $5 sudo /bin/bash /var/www/html/scripts/hdrecord.sh $1 $2" >> /var/www/html/scripts/varcron/mycron
 #install new cron file
 crontab /var/www/html/scripts/varcron/mycron
 rm /var/www/html/scripts/varcron/mycron
