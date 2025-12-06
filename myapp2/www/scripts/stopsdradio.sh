1fd71c02fd818ea8c4488dafa2fac1bbbca1db90
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 22:16:41 -0500

    Delete myapp2/www/scripts/stopsdradio.sh

--- myapp2/www/scripts/stopsdradio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/usr/bin/ps -eo pid,command | grep ffmpeg | grep "udp://" | while IFS= read -r line; do
-pid_awk=$(echo "$line" | awk '{print $1}')
-if [[ -n "$pid_awk" ]]; then
-kill -s SIGINT "$pid_awk"
-fi
-done
-echo "$pid_awk"
