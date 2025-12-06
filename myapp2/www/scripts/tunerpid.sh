fcc33ed0240f7c481826acd1a70e6413482bef21
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-05 20:02:15 -0500

    Delete myapp2/www/scripts/tunerpid.sh

--- myapp2/www/scripts/tunerpid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/usr/bin/ps -eo pid | grep rtl_fm | grep -v "grep" | while IFS= read -r line; do
-new_string=$(echo "$line" | sed 's/-s 200000 -r 48000//g')
-    echo "$new_string"
-done
-/usr/bin/ps -eo pid | grep nrsc5 | grep -v "grep" | while IFS= read -r line; do
-    echo "$line"
-done
