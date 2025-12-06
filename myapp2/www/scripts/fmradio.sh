b39f1c43e584d0b9769312d3873ee3c6a802b88c
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-06 15:54:21 -0500

    Update fmradio.sh

--- myapp2/www/scripts/fmradio.sh
+++ myapp2/www/scripts/fmradio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-command="rtl_fm -f $1M -s 200000 -r 48000 -g 49.6 2> /dev/null | ffmpeg -re -f s16le -ar 48000 -i - -af arnndn=m=/var/www/html/noisemod/rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn -c:a libmp3lame -f mpegts udp://${2}:12345 > /dev/null 2>&1 &"
+command="rtl_fm -f $1M -s 200000 -r 48000 -g 49.6 2> /dev/null | ffmpeg -re -f s16le -ar 48000 -i pipe:0 -c:v libx264 -c:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename \"segment%03d.ts\" output.m3u8 > /dev/null 2>&1 &"
 eval "$command"
