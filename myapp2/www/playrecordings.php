b89ab9b304c1b83213a350ec37535d0e3852a72c
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-06 17:49:12 -0500

    Update playrecordings.php

--- myapp2/www/playrecordings.php
+++ myapp2/www/playrecordings.php
@@ -1,1 +1,30 @@
+<?php
+$directoryPath = './var/www/html/recordings/'; 
 
+if (is_dir($directoryPath)) {
+
+    $files = scandir($directoryPath);
+
+    // Filter out '.' and '..' entries
+    $files = array_diff($files, array('.', '..'));
+
+    echo '<select name="selected_file">';
+    echo '<option value="">--Select a file--</option>';
+
+
+    foreach ($files as $file) {
+        if (is_file($directoryPath . $file)) {
+            echo '<option value="' . htmlspecialchars($file) . '">' . htmlspecialchars($file) . '</option>';
+        }
+    }
+    echo '</select>';
+} else {
+    echo '<p>Error: Directory not found.</p>';
+}
+?>
+<html>
+
+
+
+  
+</html>
