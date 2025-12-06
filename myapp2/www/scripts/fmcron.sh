54ce6e5eb050dfe9211ae25cdf611ce03c5c9654
rtlrecguy-del <rtlrecguy@gmail.com>
2025-12-06 00:49:56 -0500

    Update fmcron.sh

--- myapp2/www/scripts/fmcron.sh
+++ myapp2/www/scripts/fmcron.sh
@@ -2,7 +2,7 @@
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 crontab -l > /var/www/html/scripts/varcron/mycron
 #echo new cron into cron file
-echo -e "$5 $2 * * $3 sudo /bin/bash /var/www/html/scripts/fmrecord.sh $1 $1 $4" >> /var/www/html/scripts/varcron/mycron
+echo -e "$3 $2 * * $4 sudo /bin/bash /var/www/html/scripts/fmrecord.sh $1 $5" >> /var/www/html/scripts/varcron/mycron
 #install new cron file
 crontab /var/www/html/scripts/varcron/mycron
 rm /var/www/html/scripts/varcron/mycron
